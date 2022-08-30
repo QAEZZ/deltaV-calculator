c3f9ff418c5e92a47c6a58ecdc7ce42769812644
qaezz <russian.devv@gmail.com>
2022-08-30 16:20:32 +0000

    update loki

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from colorama import init, Fore, Style
 
 menu = f"""{Fore.BLUE}∆v{Fore.RESET} = {Fore.YELLOW}Ve{Fore.RESET} * ln({Fore.MAGENTA}mi{Fore.RESET} / {Fore.CYAN}mf{Fore.RESET})
 
-∆v:     the change in velocity of the rocket (m/s)
-Ve:     the exhaust velocity (m/s)
-mi:     the initial mass of the rocket (kg)
-mf:     the final mass of the rocket (kg)
+{Fore.BLUE}∆v{Fore.RESET}:     the change in velocity of the rocket (m/s)
+{Fore.YELLOW}Ve{Fore.RESET}:     the exhaust velocity (m/s)
+{Fore.MAGENTA}mi{Fore.RESET}:     the initial mass of the rocket (kg)
+{Fore.CYAN}mf{Fore.RESET}:     the final mass of the rocket (kg)
 
-This formula assumes a constant exhaust velocity.
+{Style.DIM}This formula assumes a constant exhaust velocity.{Style.RESET_ALL}
         """
 
 
@@ -31,25 +31,26 @@ class Main():
     def get_values(self):
         try:
             print(menu)
-            print("Ve*ln(mi/mf)")
-            self.Ve = int(input("Ve (m/s): "))
+            print(f"{Fore.YELLOW}Ve{Fore.RESET}*ln({Fore.MAGENTA}mi{Fore.RESET}/{Fore.CYAN}mf{Fore.RESET})")
+            self.Ve = int(input(f"{Fore.YELLOW}Ve{Fore.RESET} (m/s): "))
             clear()
             print(menu)
-            print(f"{self.Ve}*ln(mi/mf)")
-            self.mi = int(input("mi (kg): "))
+            print(f"{Fore.YELLOW}{self.Ve}{Fore.RESET}*ln({Fore.MAGENTA}mi{Fore.RESET}/{Fore.CYAN}mf{Fore.RESET})")
+            self.mi = int(input(f"{Fore.MAGENTA}mi{Fore.RESET} (kg): "))
             clear()
             print(menu)
-            print(f"{self.Ve}*ln({self.mi}/mf)")
-            self.mf = int(input("mf (kg): "))
+            print(f"{Fore.YELLOW}{self.Ve}{Fore.RESET}*ln({Fore.MAGENTA}{self.mi}{Fore.RESET}/{Fore.CYAN}mf{Fore.RESET})")
+            self.mf = int(input(f"{Fore.CYAN}mf{Fore.RESET} (kg): "))
             clear()
             print(menu)
-            print(f"{self.Ve}*ln({self.mi}/{self.mf})")
+            print(f"{Fore.YELLOW}{self.Ve}{Fore.RESET}*ln({Fore.MAGENTA}{self.mi}{Fore.RESET}/{Fore.CYAN}{self.mf}{Fore.RESET})")
         except ValueError:
             print("Need an integer!")
+            exit()
 
     def resolve_deltav(self):
         self.deltaV = self.Ve*ln(self.mi/self.mf)
-        print(f"∆v: {self.deltaV:.2f} m/s")
+        print(f"{Fore.BLUE}∆v{Fore.RESET}: {self.deltaV:.2f} m/s")
 
 """
 try:
